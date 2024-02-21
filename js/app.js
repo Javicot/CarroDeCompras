54b7b9a505fc7ec6c197de504b27d2dadc0c7145
Javier Tintaya Cortez <101199156+Javicot@users.noreply.github.com>
2024-02-21 16:27:28 -0400

    commit desde vscode

--- js/app.js
+++ js/app.js
@@ -1,23 +1,20 @@
-//Variables
-const carrito = document.querySelector('#carrito');
-const contenedorCarrito = document.querySelector('#lista-carrito tbody');
-const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
-const listaCursos = document.querySelector('#lista-cursos');
-
-cargarEventListeners()
-function cargarEventListeners() {
-    //Cuando agregas un curso presionando "Agregar al carrito"
-    listaCursos.addEventListener('click', agregarCurso);
-}
+//variables
+const carrito = document.querySelector("#carrito");
+const contenedorCarrito = document.querySelector("#lista-carrito tbody");
+const listaCursos = document.querySelector("#lista-cursos");
+const vaciarCarritoBtn = document.querySelector("#vaciar-carrito");
 
 //Funciones
-function agregarCurso(e) {
-    e.preventDefault();
-    if (e.target.classList.contains('agregar-carrito')) {
-    console.log(e.target);
-    }
+cargarEventListeners();
+function cargarEventListeners() {
+  // Cuando agregas un curso presionando "Agregar al Carrito"
+  listaCursos.addEventListener("click", agregarCurso);
 }
-//lee el contenido del HTML al que le dimos click y extrae la informacion del curso
-function leerDatosCurso(curso) {
-    console.log(curso);
+
+function agregarCurso(e) {
+  e.preventDefault();
+  if (e.target.classList.contains("agregar-carrito")) {
+    const curso = e.target.parentElement.parentElement;
+    leerDatosCurso(curso);
+  }
 }
